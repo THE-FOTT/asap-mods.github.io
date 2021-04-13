884870c808097ef64cc4f80de70693d5c3b11f2a
asap-mods <82436463+asap-mods@users.noreply.github.com>
2021-04-13 14:07:40 +0300

    Add files via upload

--- js/comp.js
+++ js/comp.js
@@ -5,22 +5,22 @@ let news = [
     {
         id: 1,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/ada\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Ada <i class=\" style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/ada\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Ada <i class=\" style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
     },
     {
         id: 2,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/assembly\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Assembly <i class=\" style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/assembly\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Assembly <i class=\" style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
     },
     {
         id: 3,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/c#\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">C# <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/c#\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">C# <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
     },
     {
         id: 4,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/c++(gcc)\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">C++(gcc) <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/c++(gcc)\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">C++(gcc) <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
     },
 ]
 
--- js/repos.js
+++ js/repos.js
@@ -5,49 +5,49 @@ let repos = [
     {
         id: 1,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/ada\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Ada <i class=\" style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/ada\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Ada <i class=\" style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
         url: "https://asapmods.github.io/compile/ada"    
     },
     {
         id: 2,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/assembly\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Assembly <i class=\" style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/assembly\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Assembly <i class=\" style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
         url: "https://asapmods.github.io/compile/assembly"
     },
     {
         id: 3,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/c%23\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">C# <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/c%23\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">C# <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
         url: "https://asapmods.github.io/compile/c%23"
     },
     {
         id: 4,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/c++(gcc)\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">C++(gcc) <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/c++(gcc)\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">C++(gcc) <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
         url: "https://asapmods.github.io/compile/c++(gcc)"
     },
     {
         id: 5,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/lua\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Lua <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/lua\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Lua <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
         url: "https://asapmods.github.io/compile/lua"
     },
     {
         id: 6,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/node-js\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Node.js <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/node-js\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Node.js <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
         url: "https://asapmods.github.io/compile/node-js"
     },
     {
         id: 7,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/Pascal\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Pascal <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/Pascal\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Pascal <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
         url: "https://asapmods.github.io/compile/Pascal"
     },
     {
         id: 8,
         title: "Online compile",
-        desc: "Онлайн компилятор языка <a href=\"https://asapmods.github.io/compile/python\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Python <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+        desc: "Онлайн компилятор языка <a href=\"https://asap-mods.github.io/compile/python\" style=\"color: rgb(255, 7, 112); text-decoration: none; font-weight: bold;\">Python <i class=\"style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
         url: "https://asapmods.github.io/compile/python"
     },
 ]
